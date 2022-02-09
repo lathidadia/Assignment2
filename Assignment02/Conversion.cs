f1ebf687979b774198cd3b04ea7e63bb30710ca2
lathidadia <17485874+lathidadia@users.noreply.github.com>
2022-02-09 13:22:50 -0500

    Added test project and 3 tests

--- Assignment02/Conversion.cs
+++ Assignment02/Conversion.cs
@@ -24,27 +24,32 @@ namespace Assignment02
         public double ConvertCelciusToFahrenheit()
         {
             double result = (x * ((double) 9 / 5)) + 32;
-            return result;
+            return Math.Round(result, 2, MidpointRounding.AwayFromZero);
         }
         public double ConvertCelciusToKelvin()
         {
-            return (double)x + 273.15;
+            double result = (double)x + 273.15;
+            return Math.Round(result, 2, MidpointRounding.AwayFromZero);
         }
         public double ConvertKelvinToFahrenheit()
         {
-            return (double)9 / 5 * (x - 273) + 32;
+            double result = (double)9 / 5 * (x - 273) + 32;
+            return Math.Round(result, 2, MidpointRounding.AwayFromZero);
         }
         public double ConvertKelvinToCelsius()
         {
-            return x - 273.15;
+            double result =  x - 273.15;
+            return Math.Round(result, 2, MidpointRounding.AwayFromZero);
         }
         public double ConvertFahrenheitToKelvin()
         {
-            return (x + 459.67) * (double)5 / 9;
+            double result = (x + 459.67) * (double)5 / 9;
+            return Math.Round(result, 2, MidpointRounding.AwayFromZero);
         }
         public double ConvertFahrenheitToCelsius()
         {
-            return (x - 32) * (double)5 / 9;
+            double result =  (x - 32) * (double)5 / 9;
+            return Math.Round(result, 2, MidpointRounding.AwayFromZero);
         }
     }
 }
