5df245a7b63a0cc2e3b19c9b202dbd32d1607559
lathidadia <17485874+lathidadia@users.noreply.github.com>
2022-02-09 13:57:42 -0500

    Added test project and 3 tests

--- Assignment02.Tests/ConversionTest.cs
+++ Assignment02.Tests/ConversionTest.cs
@@ -46,5 +46,85 @@ namespace Assignment02.Tests
             Assert.AreEqual(50, result);
 
         }
+
+        [Test]
+        public void ConvertCeConvertCelciusToKelvinTest1()
+
+        {
+
+            //ARRANGE
+            Conversion conversion = new Conversion();
+            //ACT
+            double result = conversion.ConvertCelciusToKelvin();
+            //ASSERT
+            Assert.AreEqual(274.15, result);
+
+        }
+        [Test]
+        public void ConvertCeConvertCelciusToKelvinTest2()
+
+        {
+
+            //ARRANGE
+            Conversion conversion = new Conversion(10);
+            //ACT
+            double result = conversion.ConvertCelciusToKelvin();
+            //ASSERT
+            Assert.AreEqual(283.15, result);
+
+        }
+        [Test]
+        public void ConvertCeConvertCelciusToKelvinTest3()
+
+        {
+
+            //ARRANGE
+            Conversion conversion = new Conversion(-15);
+            //ACT
+            double result = conversion.ConvertCelciusToKelvin();
+            //ASSERT
+            Assert.AreEqual(258.15, result);
+
+        }
+
+        [Test]
+        public void ConvertFahrenheitToCelsiusTest1()
+
+        {
+
+            //ARRANGE
+            Conversion conversion = new Conversion(0);
+            //ACT
+            double result = conversion.ConvertFahrenheitToCelsius();
+            //ASSERT
+            Assert.AreEqual(-17.78, result);
+
+        }
+        [Test]
+        public void ConvertFahrenheitToCelsiusTest2()
+
+        {
+
+            //ARRANGE
+            Conversion conversion = new Conversion(2000);
+            //ACT
+            double result = conversion.ConvertFahrenheitToCelsius();
+            //ASSERT
+            Assert.AreEqual(1093.33, result);
+
+        }
+        [Test]
+        public void ConvertFahrenheitToCelsiusTest3()
+
+        {
+
+            //ARRANGE
+            Conversion conversion = new Conversion(50);
+            //ACT
+            double result = conversion.ConvertFahrenheitToCelsius();
+            //ASSERT
+            Assert.AreEqual(10, result);
+
+        }
     }
 }
